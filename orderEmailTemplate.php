604b2a8278236d26701599a6ed787662b71cd383
bgaheer <baljeetgaheer@gmail.com>
2019-03-20 11:47:49 +0530

    c

---
+++ orderEmailTemplate.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="1https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+	<div
+		style="background-color: grey; width: 100%; color: #676a6c; font-family: open sans, Helvetica Neue, Helvetica, Arial, sans-serif">
+		<div
+			style="background-color: white; margin: auto; max-width: 600px; padding: 0px 15px 0px 15px">
+			<div
+				style="padding: 15px; background-color: #DAA520; color: white; margin: 0px -15px 0px -15px;">
+				<h1 style="margin-top: 0px; margin-bottom: 20px;">
+					<img
+						src="https://1ceipl3l02ez252khj15lgak-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/logo-white-small.png"
+						style="float: left;">
+				</h1>
+				<p align="right" style="margin: 0px;">
+					<a href="https://www.flydining.com/"
+						style="color: #fff; text-decoration: none;">www.myhealthsolutionz.com</a>
+				</p>
+			</div>
+			<div style="margin-top: 20px;display:flex;padding:15px;">
+				<div style="padding:20px 15px;max-width:100%;width:100%">
+					<p style="margin: 0px; font-size: 16px;">A new Order has been received on <i>25th March 2019 01:30PM</i> with</p>
+					<p style="font-size: 24px; font-weight: bold; margin: 0px; color: #000;">Order Id: 125C4E</p>
+				</div>
+			</div>
+			<div style="margin: 20px 0 0;display:flex">
+				<div style="border-right: 1px solid #f1f1f1;max-width:50%;padding:0px 15px;">
+					<h3 style="color: #000;">Batra Marketing, Ludhiana</h3>
+					<p style="margin: 0px;">Kesar Complex, Adjoining PVR, Malhar Road,
+						Ludhiana - 141001,Punjab,  India</p>
+					<p style="margin: 0px;">
+						<span style="font-weight: bold;">GST#:</span> 29ADHFS4111J1ZY
+					</p>
+					<p>
+						Mobile: 9814600356<br>
+						Email: info@customeremailid.com<br>
+					</p>
+					<p style="font-size:10pt">
+						Customer comments goes here.
+					</p>
+					
+				</div>
+				<div style="margin-bottom: 20px;max-width:50%;padding:0px 15px;">
+					<p style="font-size: 18px; color: #000; margin-bottom: 0px;">
+						Rs. 1,25,000.00/-				
+					</p>
+					<p style="font-size: 14px; margin-bottom: 0px;line-height:20px">
+						<span style="color:navy">Cash: Rs 10,000/-</span><br>
+						<span style="color:navy">Cheque: Rs 25,000/-</span><br>
+						<span style="color:red">Cash: Rs 10,000/- (28th March 2019)</span><br>
+						<span style="color:red">Pending: Rs. 25,000.00/-</span>				
+					</p>
+					<p style="font-size:14px">Processed by:<br> Anup Sharma - 9814012345</p>
+					
+				</div>
+			</div>
+
+			<div style="margin: 0; background: #f1f1f1; padding: 20px 20;display:flex">
+				<div style="width:100%">
+					<p	style="margin: 0 0 10px 0; font-weight: bold; border-bottom: 1px silver solid;">Order
+						Summary</p>
+					<table style="text-align:left;font-size:12px;vertical-align:top;border-bottom:1px silver solid;width:100%">
+						<tr>
+							<th width="75%" style="text-align:left;vertical-align:top">Item</th>
+							<th width="10%" style="text-align:right;vertical-align:top">Rate</th>
+							<th width="5%" style="text-align:right;vertical-align:top">Qty</th>
+							<th width="10%" style="text-align:right;vertical-align:top">Amount</th>
+						</tr>
+						<tr>
+							<td width="75%" style="text-align:left;vertical-align:top">Iso Whey 5lbs, Chocolate, Muscle Technology</td>
+							<td width="10%" style="text-align:right;vertical-align:top">4760.50</td>
+							<td width="5%" style="text-align:right;vertical-align:top">2</td>
+							<td width="15%" style="text-align:right;vertical-align:top">9521.00</td>
+						</tr>
+						<tr>
+							<td width="75%" style="text-align:left;vertical-align:top">Iso Whey 2lbs, Chocolate, Muscle Technology</td>
+							<td width="10%" style="text-align:right;vertical-align:top">4760.50</td>
+							<td width="5%" style="text-align:right;vertical-align:top">2</td>
+							<td width="15%" style="text-align:right;vertical-align:top">9521.00</td>
+						</tr>
+					</table>		
+					<div style="display:flex;width:100%">
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:left">
+							<p style="color: #000; font-size: 16px; margin: 0px;">Gross Total</p>
+						</div>
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:right;">
+							<p style="color: #000; font-size: 16px; text-align: right; margin: 0px;">9998.00/-</p>
+						</div>
+					</div>
+					<div style="display:flex;width:100%;border-bottom:1px silver solid;padding-bottom:10px;">
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:left">
+							<p style="color:red; font-size: 16px; margin: 0px;">Discount</p>
+						</div>
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:right;">
+							<p style="color:red; font-size: 16px; text-align: right; margin: 0px;">500.00/-</p>
+						</div>
+					</div>
+					<div style="display:flex;width:100%">
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:left">
+							<p
+								style="font-weight: bold; color: #000; font-size: 21px; margin: 0px;">Net Amount</p>
+						</div>
+						<div style="width:50%;padding:10px 0px 0px 0px;text-align:right;">
+							<p
+								style="font-weight: bold; color: #000; font-size: 21px; text-align: right; margin: 0px;">Rs.
+								9998.00/-</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<p
+				style="background: #DAA520; margin-top: 1 0px; color: #000; padding: 10px;"
+				align="center">
+				Notifications are generated from online portal.
+			</p>
+
+
+
+
+
+		</div>
+	</div>
+</body>
+</html>
