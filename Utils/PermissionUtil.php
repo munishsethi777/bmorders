00d618067ec243e9f3ea63e53e4ed00f85450578
munishsethi777 <munishsethi777@gmail.com>
2019-03-27 11:39:10 +0530

    permission util

--- Utils/PermissionUtil.php
+++ Utils/PermissionUtil.php
@@ -1,6 +1,12 @@
 <?php
 class PermissionUtil{
-	private static $repersentativePermissions =  array(0=>'dashboard.php',1=>'showCustomers.php',2=>'createCustomer.php',3=>'showOrders.php',4=>'createOrder.php');
+	private static $repersentativePermissions =  array(0=>'dashboard.php',
+			1=>'showCustomers.php',
+			2=>'createCustomer.php',
+			3=>'showOrders.php',
+			4=>'createOrder.php',
+			5=>'createOrderPayment.php'
+	);
 	public static function isAuthRep($pageUrl){
 		if(in_array($pageUrl, self::$repersentativePermissions)){
 			return true;
